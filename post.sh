4ced042e67d90fb5056e80077b492ecbf9095ecb
Jett Wang <jamiesun.net@gmail.com>
2024-02-23 22:35:19 +0800

    修复错误处理逻辑

---
+++ post.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 这个脚本用来创建新的 blog 文章
+#
+# 文件名模板: <yyyy-mm-dd>-<title>.md
+#
+# 内容初始化模板格式
+# ---
+# layout: post
+# title:  "<title>"
+# date:   2024-02-08 21:34:52 +0800
+# categories:
+# ---
